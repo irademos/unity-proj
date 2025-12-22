1bf917709c6df990376344181b35fa9da3a7372e
unknown <irademos@gmail.com>
2025-12-22 10:36:00 -0500

    connect button

--- Assets/Scripts/UI/NetBootstrap.cs
+++ Assets/Scripts/UI/NetBootstrap.cs
@@ -1,6 +1,7 @@
 using FishNet.Managing;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using FishNet.Transporting.Tugboat;
 
 public class NetBootstrap : MonoBehaviour
 {
@@ -28,10 +29,36 @@ public class NetBootstrap : MonoBehaviour
         SceneManager.LoadScene(gameSceneName);
     }
 
-
     public void StartClient()
     {
         networkManager.ClientManager.StartConnection();
         SceneManager.LoadScene(gameSceneName);
     }
+
+    public void Connect()
+    {
+        Debug.Log("Connect clicked");
+
+        if (networkManager == null)
+        {
+            Debug.LogError("NetworkManager not assigned");
+            return;
+        }
+
+        // Force assign Tugboat transport
+        var tugboat = networkManager.GetComponent<Tugboat>();
+        Debug.Log("Tugboat component present: " + (tugboat != null));
+
+        if (tugboat != null && networkManager.TransportManager.Transport == null)
+        {
+            networkManager.TransportManager.Transport = tugboat;
+            Debug.Log("Assigned Tugboat as active transport.");
+        }
+
+        Debug.Log("Active transport: " + networkManager.TransportManager.Transport);
+
+        networkManager.ClientManager.StartConnection();
+        Debug.Log($"Client started? {networkManager.ClientManager.Started}");
+    }
+
 }
